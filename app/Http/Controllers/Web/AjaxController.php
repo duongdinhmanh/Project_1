8a96015bf456526e2fa825aa7fff5e37e4df99b4
duongdinhmanh <duongdinhmanh252@gmail.com>
2018-10-19 14:33:29 +0700

    search

--- app/Http/Controllers/Web/AjaxController.php
+++ app/Http/Controllers/Web/AjaxController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 class AjaxController extends Controller
 {
-    // public function getDistrict($provinceId)
-    // {
-    //     $provinceId = Province::find($provinceId)->district->pluck('name', 'district_id');
-    //     foreach ($collection as $value) {
-
-    //     }
-    // }
+    public function getDistrict($provinceId)
+    {
+        $provinceId = Province::find($provinceId)->district->pluck('name', 'district_id');
+        foreach ($collection as $value) {
+        }
+    }
 }
